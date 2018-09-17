95f5ad3847d61d1faf0a3c7ed49ac1314ecaedc3
JoWos <40520197+JoWos@users.noreply.github.com>
2018-09-17 21:27:03 +0200

    Create Contact.js

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+var Contact = React.createClass({
+    propTyps: {
+        item: React.PropTypes.object.isRequired,
+    },
+
+    render: function () {
+        return (
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'https://png.icons8.com/dotty/50/000000/name.png'}/>
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+        )
+    },
+});
