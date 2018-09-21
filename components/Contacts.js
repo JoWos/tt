97ed0c74fe3c0146d6fa311432f787317d0bb2d5
JoWos <40520197+JoWos@users.noreply.github.com>
2018-09-21 22:59:46 +0200

    Update Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,22 +1,17 @@
 var Contacts = React.createClass({
-	propTypes: {
-		items: React.PropTypes.array.isRequired,
-	},
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
 
-	render: function() {
-		var contacts = this.props.items.map(function(contact) {
-			return 
-			/*React.createElement(Contact, {item: contact, key: contact.id});*/
-			<Contact key={contact.id} item={contact} />;
-			console.log('hej!!!');
-		});
+  render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return <Contact item={contact} key={contact.id} />
+    });
 
-		return (
-			React.createElement('ul', {className: 'contactsList'}, contacts)
-			/*<ul className={'contactsList'}>
-			
-			<div>Hej</div>
-			</ul>*/
-		);
-	}
-});
+    return (
+      <ul className='contactsList'>
+        {contacts}
+      </ul>
+    );
+  }
+})
